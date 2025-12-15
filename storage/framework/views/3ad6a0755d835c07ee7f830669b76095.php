51cea5c183914f41e4490c6dcbf7b0dc7333ae74
wasabikkk <162458394+wasabikkk@users.noreply.github.com>
2025-12-15 22:35:51 +0800

    Add files via upload

---
+++ storage/framework/views/3ad6a0755d835c07ee7f830669b76095.php
@@ -0,0 +1,8 @@
+Your email verification code is: <?php echo e($code); ?>
+
+
+This code will expire in 15 minutes.
+
+If you did not request this code, please ignore this email.
+
+<?php /**PATH /home/u692596975/domains/ccs4thyear.com/public_html/glowbook/facial-booking/resources/views/emails/verification-code.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f87e207d7cbd362efc5f0615340412e4.php
@@ -0,0 +1,8 @@
+Your password reset code is: <?php echo e($code); ?>
+
+
+This code will expire in 15 minutes.
+
+If you did not request a password reset, please ignore this email.
+
+<?php /**PATH /home/u692596975/domains/ccs4thyear.com/public_html/glowbook/facial-booking/resources/views/emails/password-reset-code.blade.php ENDPATH**/ ?>
